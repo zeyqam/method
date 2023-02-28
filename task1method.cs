7ae11a87702d22be2fa361e2c005f4c79f8b263a
zeyqam <zegamda@code.edu.az>
2023-02-28 06:09:29 +0400

    method

---
+++ task1method.cs
@@ -0,0 +1,24 @@
+﻿
+
+int[] array = {6,5,9,4,3,2,1};
+int number = 8;
+searching(array, number);
+void searching(int[]array, int number)
+{
+    for (int i = 0; i < array.Length; i++) ;
+    {
+        if (array[1] == number)
+        {
+            Console.WriteLine(number + "exist in array given");
+
+        }
+
+
+
+        else if (array[1] != number)  
+        {
+            Console.WriteLine(number + "doesnt exist");
+        }
+        }
+
+    }
---
+++ task2method.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+int x = Sum(12, 13, 10,78,65,102);
+
+int Sum(params object[] ListNumber)
+{
+    int max  = 0;
+    foreach (int item in ListNumber)
+    {
+        if (item > max) max = item;
+    }
+    return max ;
+}
